01f755afeaa74c2bb026378ffe7ff8afe6f42e7e
Benjamin Inemugha <binemugha@gmail.com>
2020-11-18 15:31:33 +0100

    js CF

--- forms/contactform.js
+++ forms/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
-      action = 'contactform/contactform.php';
+      action = 'form/contactform.php';
     }
     $.ajax({
       type: "POST",
